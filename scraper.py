acb936c4ae38878a5403ff1ffa513408406aca1e
trackerboy2022 <trackerboy2022@gmail.com>
2025-02-26 22:23:18 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -16,8 +16,23 @@ def extract_table_data(url):
         A list of dictionaries, where each dictionary represents a row in the table.
     """
     try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for bad status codes
+
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Referer': 'https://www.google.com/',  # Simulate coming from a search engine
+            'Connection': 'keep-alive'
+        }
+    
+        url = 'https://www.billboard.com/artist/drake/chart-history/hot-100/'
+        response = requests.get(url, headers=headers)
+    
+        if response.status_code == 200:
+            print("Request successful!")
+            # Process the HTML content
+        else:
+            print(f"Request failed with status code: {response.status_code}")
 
         soup = BeautifulSoup(response.content, 'html.parser')
 
